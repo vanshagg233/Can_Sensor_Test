0052536443692475174a0d4324cc1af5cf1cc7e7
Vansh Aggarwal <vanshagg233@gmail.com>
2025-04-22 21:56:27 -0400

    printing serial data

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <FlexCAN_T4.h>
 
-// put function declarations here:
-int myFunction(int, int);
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
+CAN_message_t msg;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    while (!Serial);  // Wait for Serial to connect
+
+    Can0.begin();
+    Can0.setBaudRate(500000);  // Match your CAN bus speed
+
+    Serial.println("CAN2 Ready to receive...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (Can0.read(msg)) {
+        Serial.print("ID: 0x");
+        Serial.print(msg.id, HEX);
+        Serial.print("  Data: ");
+        for (int i = 0; i < msg.len; i++) {
+            Serial.printf("%02X ", msg.buf[i]);
+        }
+        Serial.println();
+    }
 }
